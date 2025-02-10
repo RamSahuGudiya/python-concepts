bba2d6394802ca1e6312bed87583a3b7259e329e
Ram sahu <ramsahu55550@gmail.com>
2025-02-10 14:47:41 +0530

    oops concept basically covered constructors and methods

---
+++ practice.py
@@ -0,0 +1,488 @@
+# ramAge = int(input("Enter Rams Age: "))
+
+# if(ramAge >=18):
+#   print('eligible for driving licence')
+
+# else:
+#   print('Not eligible')
+
+# str = "ApnaCollege"
+
+# print(str[1:4])
+
+# print(str[:4])
+
+# print(str[: ])  
+# print(str[4:len(str)])
+
+# str = "im learning python from apnaCollege"
+
+# str = "Hi im $ and one $ is equalent to 87.55 rupees"
+# print(str.endswith("app"))
+
+# print(str.capitalize())
+
+# print(str.replace("a", '@'))
+# print(str.replace("python", "javascript"))
+
+# print(str.find("p"))
+# print(str.count("a"))
+
+# fname = input("enter your name")
+
+
+# print(len(fname))
+# print(str.count("$"))
+
+
+# light = 'pink'
+
+# if(light == "red"):
+#     print('stop')
+# elif(light == "orange"):
+#     print('wait')
+# elif(light == 'green'):
+#     print('go')    
+
+# print('the light has broken')
+
+# marks = 90
+
+# if(marks>=90):
+#     print('A')
+# elif(marks<90 and marks >=80):
+#     print('B')
+# elif(marks < 80 and marks >= 70):
+#     print('C')
+# elif(marks < 70 and marks >35):
+#     print('D')
+# else:
+#     print('Fail')            
+
+
+# num = int(input("enter the number :"))
+
+# if(num % 2 == 0):
+#     print("even")
+# else:
+#     print('odd')    
+
+# a = 2.3
+
+# b = 10
+# c = 6
+
+# if(a>b and a>c):
+#     print('a is greatest')
+# elif(b > c):
+#     print('b is greatest')
+# else:
+#     print('c is greatest')        
+
+# num = 21
+
+# if(num%7 == 0):
+#     print('Divisible by seven')
+# else:
+#     print('not divisible by seven').
+
+# marks = [87, 64, 35,78,10]
+# print(marks[0])
+# print(marks[1])
+# print(len(marks))
+
+# student = ['karan', 95.4, "Delhi"]
+
+# print(student[0])
+
+# student[0]= 'ram'
+
+# print(student)
+
+# marks = [87, 64, 33, 95, 76]
+
+# print(marks[1:4])
+
+# print(marks[ : 4])
+
+# print(marks[1: len(marks)])
+# print(marks[1:])
+
+# print(marks[-3:-1])
+
+# list = ["Hi", "this", " ","Ram", "sahu"]
+# list.append(4)
+
+# print(list)
+# list.reverse()
+# list.sort(reverse=True)
+
+# print(list)
+# print(list)
+
+# list.insert(2, "is")
+
+# print(list)
+
+# list.remove(" ")
+# list.pop(2)
+# print(list)
+
+
+# movie1 = input('enter movie name')
+# movie2 = input('enter movie name')
+# movie3 = input('enter movie name')
+
+# movies = [movie1, movie2, movie3]
+# print(movies)
+
+# palindrome = ["ram", "sahu", "ram"]
+
+# copyPalindrome = palindrome.copy()
+# copyPalindrome.reverse()
+
+# if(palindrome == copyPalindrome ):
+#     print('Its a palindrome list')
+# else:
+#     print('not palindrome list')    
+
+
+# student = ('C,', 'D', 'A', 'A', 'B', 'B', 'A')
+
+# studentGrade = student.count('B')
+# print(studentGrade)
+# print('There are', studentGrade, 'with grade B')
+
+# student = ['C,', 'D', 'A', 'A', 'B', 'B', 'A']
+
+# student.sort()
+# print(student)
+
+
+# info = {
+#     "name":"Ram sahu",
+#     "marks": [88,99,91,98],
+#     "subjects":('python', 'javascript', 'react'),
+#     "age":22,     
+#     12:89.00,
+#      [33,44 ]: "haha dict used"
+#     name":'reshma'
+
+# }
+
+# print(info)
+
+# print(info['name'])
+
+# print(info['marks'])
+
+# print(info["age"])
+
+# info['age'] = 24
+
+# print(info)
+
+# print(info['age'])
+
+
+# dict = {
+# "table": ["a piece  of furniture", "list of facts abd figures"],
+#     "cat": 'a samll animal'
+# }
+
+# print(dict)
+
+# classRooms = {"python", "java", "c++", "python", "javascript", "java", "python", "java", "c++", "c"}
+
+# print("we require ",len(classRooms), "class rooms for each subject")
+
+# chem = int(input('enter chem marks : '))
+# phy = int(input('enter phy marks : '))
+# math = int(input('enter math marks : '))
+
+# marks={}
+# marks.update({"chem":chem, "phy": phy, "math": math })
+
+# print(marks)
+
+# count = 1
+
+# while count <=100:
+#     print('instruction', count)
+#     count +=1
+
+# i = 100
+
+# while i >=1:
+#     print(i)
+#     i-=1
+
+
+# n = 3
+# i =1
+# mul =1;
+# while mul <=30:
+#     print(mul)
+#     mul=n*i
+#     i+=1
+# n = 3
+# i =1
+
+# while i<=10:
+#     print(n*i)
+#     i+=1
+
+# nums = [1,4,9,16,25,36,49,64,81,100]
+
+
+# i=0
+# while i < len(nums):
+#     print(nums[i]) 
+#     i+=1
+
+# nums = (1,4,9,16,25,36,49,64,81,100)
+
+# i =0
+# n = 36
+# while i< len(nums):
+#     if(n == nums[i]):
+#         print('element at index', i)
+#         break
+#     else:
+#         print('invalid number')    
+
+#     i+=1.
+
+
+# i = 1
+# while i <= 10:
+#     if (i%2!= 0):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+
+# list = [1,2,3,4,5,6,7,8]
+
+# for ele in list:
+#     print(ele).
+
+# num = [1,4,9,16,25,36,49,64,81,100]
+
+# x = 36
+# idx = 0
+# for el in num:
+#     if(el==x):
+#         print('found the number', idx)
+
+#     idx+=1.
+
+
+# n = int(input("Enter the number : "))
+
+# for i in range(1, 10):
+#     print(n*i)
+
+# list = [1,2,3,4,5,]
+# i=0
+# sum = 1
+# while (i< len(list)):
+#     sum *=list[i]
+#     i+=1
+# print(sum)    
+
+
+# def calC_sum(a,b,c):
+#     sum = a+b+c
+#     avg = sum/3
+#     print(avg)
+
+# calC_sum(1,2,3)
+# calC_sum(3,2,4)
+
+cities = [1,2,3,4,5]
+
+# def place_len(places):
+#     print("length of the cities",len(places))
+
+
+# place_len(cities)
+
+# def place_len(places):
+#     for i in places:
+#         print(i,end=" ")
+    
+
+# place_len(cities)
+
+# def place_len(n):
+#     fact = 1
+#     for i in range(1,n+1):
+#         fact *=i
+#     print(fact )
+    
+
+# place_len(5)
+
+# def converter(usd):
+#     conversion = 83
+#     inr = usd*conversion
+#     print(inr)
+
+# converter(2)
+
+# def shuee(n):
+#     if(n==0):
+#         return
+#     print(n)
+#     shuee(n-1)
+
+# shuee(5)    
+
+# def first_sum(n):
+     
+#     if(n==0 or n==1): 
+#         return 1
+
+#     return first_sum(n-1) + n    
+
+# print(first_sum(5))    
+
+
+
+# lis = [1,2,3,4,5,6]
+
+# def show(list ,index = 0):
+#     if(index == len(list)):
+
+#         return
+#     print(list[index])
+
+#     show(list, index+1)
+
+# show(lis)
+
+# f = open('demo.txt', 'r')
+
+# data = f.read()
+
+
+# print(data)
+# print(type(data))
+
+
+# f.close()
+# f = open('demo.txt', 'a')
+
+# data = f.write("\nafter that i need to crack high paying job to feedmy family")
+# f.close()
+
+# with open("demo.txt", 'r') as f:
+#     data = f.read()
+#     print(data)
+
+# import os
+# os.remove("python.txt")
+
+# with open("demo.txt", 'w') as f:
+#     data = f.write('this is new file')
+
+
+# f = open('python.txt', "a")
+# f.write('hi everyone\n')
+# f.write('we are learning file i/o\n')
+# f.write('using python\n')
+# f.write('i like programming in java\n')
+
+
+# f = open('python.txt', 'r')
+# data = f.read()
+# new_data = data.replace('java', 'python')
+# print(new_data)
+
+# f = open('python.txt', 'w')
+# f.write(new_data)
+
+# f = open('python.txt', 'r')
+
+# data = f.read()
+
+# print(data.count('learning')== 1)
+# import os
+# os.remove("python.txt")
+
+
+# class Students:
+#     def __init__(self, fullname, marks):
+#         self.name = fullname
+#         self.marks = marks
+#         print("Hi this is", self.name, "and got", self.marks)
+
+#     def welcome(self):
+#         print("Hey",self.name, "hope you were doing well")
+    
+#     def get_marks(self):
+#         return self.marks
+
+# s1 = Students("Ram sahu", 98)
+# s2 = Students("ravi", 70)
+
+
+
+# s1.welcome()
+# s2.welcome()
+
+# print(s1.get_marks())
+# print(s2.get_marks())
+
+# class Students:
+#     # def __init__(self, name, marks1, marks2, marks3):
+#     def __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
+#     def print_average(self):
+#         sum = 0
+#         for val in marks:
+#             sum +=val
+#         avg = sum /3
+#         print(avg)
+
+
+# marks = [10, 20, 30]
+# s1 = Students('ram', marks)
+# s1.print_average()        
+
+class Account:
+    def __init__(self, balance, accNum):
+        if(balance >=0):
+            self.balance = balance
+            self.accNum = accNum
+            print("On your account" , self.accNum, "has", self.balance)
+        else:
+            print('need to  add sufficient amount')
+        
+
+    def debit(self,  amount):
+        if(self.balance >= amount):
+            self.balance -= amount
+            print('Rs', amount, "was debited, Remaining balance", self.balance)
+        else:
+            print('insufficient balance in your account')
+        
+        
+
+    def credit(self, amount):
+        self.balance += amount
+        print('Rs', amount, "was credited, now you have balance", self.balance)
+
+    def get_balance(self):
+        return self.balance
+
+
+user1 = Account(1000, 12345)
+user1.debit(100)
+user1.debit(900)
+user1.credit(10000)
+print(user1.get_balance())
+# user1.debit(-1)
